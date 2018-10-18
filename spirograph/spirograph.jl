0fb9f1e01d809cf0bd79c19170b4040bd7d076c4
Saurabh Deoras <saurabh@portworx.com>
2018-10-18 09:01:52 -0700

    spirograph

--- spirograph/spirograph.jl
+++ spirograph/spirograph.jl
@@ -4,8 +4,13 @@
 # revolutions is the number of revolutions of smaller wheel
 function spirograph(;ratio=4.0, pen=1.0, revolutions=1)
        r = 1; R = r * ratio;
-       θ = range(0, stop=revolutions*2*π, length=1024*revolutions);
+       θ = range(0, stop=revolutions*2*π, length=128*revolutions);
        x = (R-r).*cos.(θ) .+ (r*pen).*cos.(((R+r)/r)*θ);
        y = (R-r).*sin.(θ) .+ (r*pen).*sin.(((R+r)/r)*θ);
        return x, y
+end
+
+function drawSpirograph(; ratio=4.0, pen=1.0, revolutions=1)
+	x, y = spirograph(ratio=ratio, pen=pen, revolutions=revolutions)
+	plot(x, y, size=[500, 450])
 end
