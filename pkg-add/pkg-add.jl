c5efaef0976d383c0c1b68532462a4c95ea352e2
Saurabh Deoras <saurabh@portworx.com>
2018-10-25 05:34:57 -0700

    pkg add list

---
+++ pkg-add/pkg-add.jl
@@ -0,0 +1,17 @@
+using Pkg
+Pkg.add("FileIO")
+Pkg.add("LinearAlgebra")
+Pkg.add("DifferentialEquations")
+Pkg.add("ParameterizedFunctions")
+Pkg.add("CSV")
+Pkg.add("Polynomials")
+Pkg.add("DataFrames")
+Pkg.add("Statistics")
+
+using CSV
+using DataFrames
+using DifferentialEquations
+using ParameterizedFunctions
+using FileIO
+using LinearAlgebra
+using Statistics
