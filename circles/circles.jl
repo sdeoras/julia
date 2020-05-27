c24a8d7e28c8386074696a6924853dd37042b0bd
Saurabh Deoras <saurabh@diamanti.com>
2020-05-26 21:11:46 -0700

    updates to circles script
    Signed-off-by: Saurabh Deoras <saurabh@diamanti.com>
    
     Please enter the commit message for your changes. Lines starting
     with '' will be ignored, and an empty message aborts the commit.
    
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   circles/circles.jl

--- circles/circles.jl
+++ circles/circles.jl
@@ -2,37 +2,61 @@
 
 using Plots
 using Statistics
+using Colors
 
 gr()
 
 n = 5;
-x = 30 .* rand(n) .- 15;
+x = 50 .* rand(n) .- 25;
 y = 30 .* rand(n) .- 15;
 
 px = x;
 py = y;
 
 t = range(-π, π, length = 100)
-plot(0, 0, legend = :none, aspect_ratio = :equal, border = nothing, axis = nothing)
+plot(
+    0,
+    0,
+    legend = :none,
+    aspect_ratio = :equal,
+    border = :none,
+    axis = nothing,
+    background_color = :black,
+    xlim = [-25, 25],
+    ylim = [-15, 15],
+)
 
 function foo(x, y, px, py)
     for i = 1:n
         d = (x[i] .- px[i+1:end]) .^ 2 + (y[i] .- py[i+1:end]) .^ 2
-        r = sqrt(min(d...)) - 0.001
+        r = sqrt(min(d...)) - 0.4
+
+        if r < 0
+            r = 0
+        end
+
         cx = x[i] .+ r .* cos.(t)
         cy = y[i] .+ r .* sin.(t)
 
         px = [px; cx]
         py = [py; cy]
 
-        plot!(cx, cy, legend = :none, aspect_ratio = :equal, grid = false, border = nothing, axis = nothing)
+        plot!(
+            cx,
+            cy,
+            legend = :none,
+            aspect_ratio = :equal,
+            grid = false,
+            border = :none,
+            axis = nothing,
+        )
     end
     return px, py
 end
 
 function foo2(px, py)
-    for i = 1:40
-        x = 30 .* rand(n) .- 15
+    for i = 1:200
+        x = 50 .* rand(n) .- 25
         y = 30 .* rand(n) .- 15
         px, py = foo(x, y, px, py)
     end
@@ -42,5 +66,13 @@ px, py = foo(x, y, px, py)
 
 foo2(px, py)
 
-display(plot!(0, 0, legend = :none, aspect_ratio = :equal, border = nothing, axis = nothing))
+display(plot!(
+    0,
+    0,
+    legend = :none,
+    aspect_ratio = :equal,
+    border = :none,
+    axis = nothing,
+))
 savefig("circles.svg")
+savefig("circles.png")
